c16eb681c06cfa30959d74a7ae962ce710e3c52d
Nnah Nnamdi <44839686+techagentng@users.noreply.github.com>
2026-01-10 19:39:07 +0100

    setup nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,13 @@
 import Image from "next/image";
-
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
+import { Tabs, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { Button } from "@/components/ui/button";
 export default function Home() {
-
+return (
+  <main>
+    <div>
+      
+    </div>
+  </main>
+)
 }
